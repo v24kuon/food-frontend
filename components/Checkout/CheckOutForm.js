c6773307bd6292ac16b4294f5da6e08b1135f387
v24kuon <63438234+v24kuon@users.noreply.github.com>
2022-10-13 18:44:58 +0900

    ver7

--- components/Checkout/CheckOutForm.js
+++ components/Checkout/CheckOutForm.js
@@ -68,7 +68,6 @@ const CheckOutForm = () => {
           .paper {
             border: 1px solid lightgray;
             box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-            height: 550px;
             padding: 30px;
             background: #fff;
             border-radius: 6px;
--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ const Layout = (props) => {
   return (
     <div>
       <Head>
-        <title>フードデリバリーサービス</title>
+        <title>ショッピングサイト</title>
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css' />
       </Head>
       <header>
