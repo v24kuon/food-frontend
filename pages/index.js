bbc49ea509b7c11f0c2388e6729035e88eb15da6
v24kuon <63438234+v24kuon@users.noreply.github.com>
2022-10-17 19:20:56 +0900

    ver19

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { Col, Input, InputGroup, InputGroupText, Row } from 'reactstrap';
 import RestaurantList from '../components/RestaurantsList';
 import React, { useState } from 'react';
-import Three from '../components/3D/Three';
 
 const index = () => {
   const [query, setQuery] = useState('');
