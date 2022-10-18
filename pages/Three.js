48be69362dd83a394532ad198902b8f24fc21314
Ohtani <v24kuon@gmail.com>
2022-10-18 10:15:55 +0900

    ver32

--- pages/Three.js
+++ pages/Three.js
@@ -1,31 +1,85 @@
-import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh {...props} ref={mesh} scale={active ? 1.5 : 1} onClick={(event) => setActive(!active)} onPointerOver={(event) => setHover(true)} onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
+let camera;
+let scene;
+let renderer;
+let model;
+
+init();
+animate();
+
+function init() {
+  //シーンの作成
+  scene = new THREE.Scene();
+
+  //カメラの作成
+  camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 2000);
+  //カメラセット
+  camera.position.set(-20, 30, 50);
+  camera.lookAt(new THREE.Vector3(0, 10, 0));
+
+  // 滑らかにカメラコントローラーを制御する
+  const controls = new OrbitControls(camera, document.body);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.2;
+
+  //光源
+  const dirLight = new THREE.SpotLight(0xffffff, 1.5); //color,強度
+  dirLight.position.set(-20, 30, 30);
+  scene.add(dirLight);
+  const light = new THREE.AmbientLight(0xffffff, 1.0);
+  scene.add(light);
+
+  //レンダラー
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
+  renderer.setClearColor(new THREE.Color(0xffffff));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  //glbファイルの読み込み
+  const loader = new GLTFLoader();
+
+  loader.load(
+    'http://gtgshare006.xsrv.jp/3d/wallet.glb',
+    function (gltf) {
+      model = gltf.scene;
+      model.traverse((object) => {
+        //モデルの構成要素
+        if (object.isMesh) {
+          //その構成要素がメッシュだったら
+          object.material.trasparent = true; //透明許可
+          object.material.opacity = 0.8; //透過
+          object.material.depthTest = true; //陰影で消える部分
+        }
+      });
+      scene.add(model);
+    },
+    undefined,
+    function (e) {
+      console.error(e);
+    }
   );
+
+  document.getElementById('WebGL-output').appendChild(renderer.domElement);
 }
 
-const Home = () => (
-  <div style={{ width: '100vw', height: '100vh' }}>
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
-  </div>
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+document.body.addEventListener(
+  'touchmove',
+  (e) => {
+    if (e.touches.length > 1) {
+      e.preventDefault();
+    }
+  },
+  { passive: false }
 );
+
+const Home = () => <div style={{ width: '100vw', height: '100vh' }} id='WebGL-output'></div>;
 export default Home;
