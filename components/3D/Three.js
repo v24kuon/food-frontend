19613769d5f707c51649c460243addb8a216cbff
Ohtani <v24kuon@gmail.com>
2022-10-18 15:46:47 +0900

    ver50

--- components/3D/Three.js
+++ components/3D/Three.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from '../lib/orbitcontrols';
-import { GLTFLoader } from '../lib/gltf';
+import { OrbitControls } from '../../lib/orbitcontrols';
+import { GLTFLoader } from '../../lib/gltf';
 
 const Three = () => {
   let canvas;
