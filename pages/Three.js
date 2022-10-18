d550402c2b3b6de88e61fc9cea719a62ec59439a
Ohtani <v24kuon@gmail.com>
2022-10-18 09:44:32 +0900

    ver25

--- pages/Three.js
+++ pages/Three.js
@@ -1,73 +1,30 @@
-import React, { useRef } from 'react';
-import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
-import { CubeTextureLoader, CubeCamera, WebGLCubeRenderTarget, RGBFormat, LinearMipmapLinearFilter } from 'three';
-import { OrbitControls } from '../lib/orbitcontrols';
+import { useState, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 
-extend({ OrbitControls });
+const Box = () => {
+  const ref = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
 
-const CameraControls = () => {
-  // Get a reference to the Three.js Camera, and the canvas html element.
-  // We need these to setup the OrbitControls class.
-  // https://threejs.org/docs/#examples/en/controls/OrbitControls
-
-  const {
-    camera,
-    gl: { domElement },
-  } = useThree();
-
-  // Ref to the controls, so that we can update them on every frame using useFrame
-  const controls = useRef();
-  useFrame(() => controls.current.update());
-  return <orbitControls ref={controls} args={[camera, domElement]} autoRotate={true} enableZoom={false} />;
-};
-
-// Loads the skybox texture and applies it to the scene.
-function SkyBox() {
-  const { scene } = useThree();
-  const loader = new CubeTextureLoader();
-  // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
-  const texture = loader.load(['/1.jpg', '/2.jpg', '/3.jpg', '/4.jpg', '/5.jpg', '/6.jpg']);
-
-  // Set the scene background property to the resulting texture.
-  scene.background = texture;
-  return null;
-}
-
-// Geometry
-function Sphere() {
-  const { scene, gl } = useThree();
-  // The cubeRenderTarget is used to generate a texture for the reflective sphere.
-  // It must be updated on each frame in order to track camera movement and other changes.
-  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
-    format: RGBFormat,
-    generateMipmaps: true,
-    minFilter: LinearMipmapLinearFilter,
-  });
-  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
-  cubeCamera.position.set(0, 0, 0);
-  scene.add(cubeCamera);
-
-  // Update the cubeCamera with current renderer and scene.
-  useFrame(() => cubeCamera.update(gl, scene));
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  }, []);
 
   return (
-    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
-      <directionalLight intensity={0.5} />
-      <sphereGeometry attach='geometry' args={[2, 32, 32]} />
-      <meshBasicMaterial attach='material' envMap={cubeCamera.renderTarget.texture} color='white' roughness={0.1} metalness={1} />
+    <mesh ref={ref} onPointerOver={() => setIsHovered(true)} onPointerOut={() => setIsHovered(false)}>
+      <boxBufferGeometry args={isHovered ? [1.2, 1.2, 1.2] : [1, 1, 1]} />
+      <meshLambertMaterial color={isHovered ? 0x44c2b5 : 0x9178e6} />
     </mesh>
   );
-}
+};
 
-// Lights
-function Three() {
+export default function App() {
   return (
-    <Canvas className='canvas' style={{ height: '40vh' }}>
-      <CameraControls />
-      <Sphere />
-      <SkyBox />
+    <Canvas dpr={2}>
+      <color attach='background' args={[0xf5f3fd]} />
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={0.5} position={[-10, 10, 10]} />
+      <Box />
     </Canvas>
   );
 }
-
-export default Three;
