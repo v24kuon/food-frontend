060e061c1d7cf9af4dfede4bda6f3d18c1dcef4f
Ohtani <v24kuon@gmail.com>
2022-10-18 15:34:20 +0900

    ver47

--- pages/restaurants.js
+++ pages/restaurants.js
@@ -32,54 +32,52 @@ const Restaurants = (props) => {
   if (error) return <h1>レストランの読み込みに失敗しました</h1>;
   if (data) {
     const { restaurant } = data;
-    if ((restaurant.name = 'Purse Workshop')) {
-      return (
-        <>
-          <h1 style={{ marginTop: '1em' }}>{restaurant.name}</h1>
-          <Row>
-            {restaurant.dishes.map((dish) => (
-              <Col xs='12' sm='6' md='4' key={dish.id} style={{ padding: 0 }}>
-                <Card style={{ margin: '0 10px' }}>
-                  <CardImg src={`${dish.image.url}`} top={true} style={{ height: 250 }} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardTitle>{dish.description}</CardTitle>
-                  </CardBody>
-                  <div className='card-footer'>
-                    <Button outline color='primary' onClick={() => appContext.addItem(dish)}>
-                      + カートに入れる
-                    </Button>
-                  </div>
-                </Card>
-              </Col>
-            ))}
-            <style jsx>
-              {`
-                a {
-                  color: white;
-                }
-                a:link {
-                  text-decoration: none;
-                  color: white;
-                }
-                a:hover {
-                  color: white;
-                }
-                .card-colums {
-                  column-count: 3;
-                }
-              `}
-            </style>
-            <Col xs='12' sm='6' md='4' style={{ padding: 0 }}>
-              <div>
-                <Cart />
-              </div>
+    return (
+      <>
+        <h1 style={{ marginTop: '1em' }}>{restaurant.name}</h1>
+        {(restaurant.name = 'Purse Workshop' ? <p>財布</p> : <p>違う</p>)}
+        <Row>
+          {restaurant.dishes.map((dish) => (
+            <Col xs='12' sm='6' md='4' key={dish.id} style={{ padding: 0 }}>
+              <Card style={{ margin: '0 10px' }}>
+                <CardImg src={`${dish.image.url}`} top={true} style={{ height: 250 }} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardTitle>{dish.description}</CardTitle>
+                </CardBody>
+                <div className='card-footer'>
+                  <Button outline color='primary' onClick={() => appContext.addItem(dish)}>
+                    + カートに入れる
+                  </Button>
+                </div>
+              </Card>
             </Col>
-          </Row>
-        </>
-      );
-    } else {
-    }
+          ))}
+          <style jsx>
+            {`
+              a {
+                color: white;
+              }
+              a:link {
+                text-decoration: none;
+                color: white;
+              }
+              a:hover {
+                color: white;
+              }
+              .card-colums {
+                column-count: 3;
+              }
+            `}
+          </style>
+          <Col xs='12' sm='6' md='4' style={{ padding: 0 }}>
+            <div>
+              <Cart />
+            </div>
+          </Col>
+        </Row>
+      </>
+    );
   } else {
     return <h1>レストランが見つかりませんでした。</h1>;
   }
