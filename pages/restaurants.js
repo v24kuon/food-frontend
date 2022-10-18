9d1683630d996c58069da58a17ac5f1d50b79ad7
Ohtani <v24kuon@gmail.com>
2022-10-18 15:27:29 +0900

    ver46

--- pages/restaurants.js
+++ pages/restaurants.js
@@ -30,54 +30,56 @@ const Restaurants = (props) => {
   const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES, { variables: { id: router.query.id } });
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>レストランの読み込みに失敗しました</h1>;
-  console.log(data);
   if (data) {
     const { restaurant } = data;
-    return (
-      <>
-        <h1 style={{ marginTop: '1em' }}>{restaurant.name}</h1>
-        <Row>
-          {restaurant.dishes.map((dish) => (
-            <Col xs='12' sm='6' md='4' key={dish.id} style={{ padding: 0 }}>
-              <Card style={{ margin: '0 10px' }}>
-                <CardImg src={`${dish.image.url}`} top={true} style={{ height: 250 }} />
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardTitle>{dish.description}</CardTitle>
-                </CardBody>
-                <div className='card-footer'>
-                  <Button outline color='primary' onClick={() => appContext.addItem(dish)}>
-                    + カートに入れる
-                  </Button>
-                </div>
-              </Card>
+    if ((restaurant.name = 'Purse Workshop')) {
+      return (
+        <>
+          <h1 style={{ marginTop: '1em' }}>{restaurant.name}</h1>
+          <Row>
+            {restaurant.dishes.map((dish) => (
+              <Col xs='12' sm='6' md='4' key={dish.id} style={{ padding: 0 }}>
+                <Card style={{ margin: '0 10px' }}>
+                  <CardImg src={`${dish.image.url}`} top={true} style={{ height: 250 }} />
+                  <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardTitle>{dish.description}</CardTitle>
+                  </CardBody>
+                  <div className='card-footer'>
+                    <Button outline color='primary' onClick={() => appContext.addItem(dish)}>
+                      + カートに入れる
+                    </Button>
+                  </div>
+                </Card>
+              </Col>
+            ))}
+            <style jsx>
+              {`
+                a {
+                  color: white;
+                }
+                a:link {
+                  text-decoration: none;
+                  color: white;
+                }
+                a:hover {
+                  color: white;
+                }
+                .card-colums {
+                  column-count: 3;
+                }
+              `}
+            </style>
+            <Col xs='12' sm='6' md='4' style={{ padding: 0 }}>
+              <div>
+                <Cart />
+              </div>
             </Col>
-          ))}
-          <style jsx>
-            {`
-              a {
-                color: white;
-              }
-              a:link {
-                text-decoration: none;
-                color: white;
-              }
-              a:hover {
-                color: white;
-              }
-              .card-colums {
-                column-count: 3;
-              }
-            `}
-          </style>
-          <Col xs='12' sm='6' md='4' style={{ padding: 0 }}>
-            <div>
-              <Cart />
-            </div>
-          </Col>
-        </Row>
-      </>
-    );
+          </Row>
+        </>
+      );
+    } else {
+    }
   } else {
     return <h1>レストランが見つかりませんでした。</h1>;
   }
