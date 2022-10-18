14cd93099a199a643741c8ff21733cd8f149af6b
Ohtani <v24kuon@gmail.com>
2022-10-18 10:17:45 +0900

    ver33

--- pages/Three.js
+++ pages/Three.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 let camera;
 let scene;
